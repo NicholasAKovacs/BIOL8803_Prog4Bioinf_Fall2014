d04834315d880cf6720f7ca36437cce9b2adbab4
Nicholas Kovacs <nattila.kovacs@gmail.com>
2015-04-13 02:02:13 -0400

    Update week8_1.pl

--- week8_1.pl
+++ week8_1.pl
@@ -3,16 +3,16 @@ use strict;
 
 #Set up variables
 my $num=1;
-my @array;
-my $sum;
+my @array=();
+my $sum=0;
 
 #If the number is even, add to back of array, add to front of array if even
 while ($num != 0) {
 	print "Please input a number: \n";
 	$num = <STDIN>;
-        if ($num>0) {
+        if ($num%2 == 0) {
                 push (@array, $num);
-        } elsif ($num<0) {
+        } elsif ($num%2 == 1) {
                 unshift (@array, $num);
         }
 }
@@ -20,7 +20,7 @@ print "You entered zero, program terminated\n";
 
 #Print the array on one line
 chomp @array;
-print join('.',@array), "\n";
+print @array, "\n";
 
 #Print the sum
 foreach my $n (@array) {
