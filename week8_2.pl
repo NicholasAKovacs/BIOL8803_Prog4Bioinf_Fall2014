1c10927a1704d0a69a52e7354a9f83335e12b32d
Nicholas Kovacs <nattila.kovacs@gmail.com>
2015-04-13 02:03:00 -0400

    Update week8_2.pl

--- week8_2.pl
+++ week8_2.pl
@@ -4,51 +4,49 @@ use strict;
 #Open file, which is the first argument
 open FILE, $ARGV[0];
 
-#Set up the global variables
-my @Name;
-my @Class;
-my @Family;
-my %countName;
-my %countClass;
-my %countFamily;
+#Set up the variables
+my @col0=();
+my @col1=();
+my @col2=();
+my %count0;
+my %count1;
+my %count2;
 
 #Divide the lines into the columns and add that column input to an array
-my $skipline1;
 while ( my $line = <FILE>) {
-	$skipline1++ or next;
 	chomp($line);
-	my @fields=split /\t/, $line;
-	push(@Name, $fields[10]);
-	push(@Class, $fields[11]);
-	push(@Family, $fields[12]);
+	my @fields=split / /, $line;
+	push(@col0, $fields[0]);
+	push(@col1, $fields[1]);
+	push(@col2, $fields[2]);
 }
 
 #Count each unique word of the arrays
-foreach my $entry0 (@Name) {
-	$countName{$entry0}++;
+foreach my $entry0 (@col0) {
+	$count0{$entry0}++;
 }
 
-foreach my $entry1 (@Class) {
-        $countClass{$entry1}++;
+foreach my $entry1 (@col1) {
+        $count1{$entry1}++;
 }
 
-foreach my $entry2 (@Family) {
-        $countFamily{$entry2}++;
+foreach my $entry2 (@col2) {
+        $count2{$entry2}++;
 }
 
 #Output: "word --> count"
-print "\nrepName:\n";
-foreach my $key (keys %countName) {
-	print "$key --> $countName{$key}\n";
+print "\nColumn 1:\n";
+foreach my $key (keys %count0) {
+	print "$key --> $count0{$key}\n";
 }
 
-print "\nrepClass:\n";
-foreach my $key (keys %countClass) {
-        print "$key --> $countClass{$key}\n";
+print "\nColumn 2:\n";
+foreach my $key (keys %count1) {
+        print "$key --> $count1{$key}\n";
 }
 
-print "\nrepFamily:\n";
-foreach my $key (keys %countFamily) {
-        print "$key --> $countFamily{$key}\n";
+print "\nColumn 3:\n";
+foreach my $key (keys %count2) {
+        print "$key --> $count2{$key}\n";
 }
 
